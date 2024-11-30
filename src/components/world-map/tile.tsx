f1efd79cd929a8bdc2de3e13ce83322ee0e833a5
Neil Murphy <barrymun@tcd.ie>
2024-11-30 17:09:15 +0000

    adding mountain png

--- src/components/world-map/tile.tsx
+++ src/components/world-map/tile.tsx
@@ -1,6 +1,7 @@
-import { Box } from "@mantine/core";
+import { Box, Image } from "@mantine/core";
 import { FC } from "react";
 
+import mountain from "assets/img/mountain.png";
 import { WorldMap } from "db";
 
 import classes from "./tile.module.scss";
@@ -12,9 +13,17 @@ interface TileProps {
 const Tile: FC<TileProps> = (props) => {
   const { tile } = props;
 
+  const getImageSrc = () => {
+    if (tile.tileType === "mountain") {
+      return mountain;
+    }
+    return "";
+  };
+
   return (
     <Box className={classes.tile}>
-      {tile.x}, {tile.y}
+      {/* {tile.x}, {tile.y} */}
+      <Image src={getImageSrc()} alt={`Tile ${tile.x}, ${tile.y}`} draggable={false} />
     </Box>
   );
 };
